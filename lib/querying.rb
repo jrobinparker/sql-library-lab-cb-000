a1babc1806f2e0639d1c6e3ebe2fb72479ce62e9
Jeremy Parker <jrobinparker@gmail.com>
2018-06-18 16:48:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  return "SELECT Authors.name, Subgenres.name FROM Authors INNER JOIN Subgenres ON Series.author_id = Author.id;"
+  return "SELECT Authors.name, Subgenres.name FROM Authors JOIN Series ON Series.author_id = Author.id INNER JOIN Subgenres ON Subgenres.id = Series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
